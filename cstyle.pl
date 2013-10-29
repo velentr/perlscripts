de7b702c0619f7bdf2364bb2e999e1fcfd8f8322
Brian Kubisiak <bkubisiak.git@gmail.com>
2013-10-29 11:54:07 -0700

    Shortened cstyle

--- cstyle.pl
+++ cstyle.pl
@@ -2,28 +2,17 @@
 
 while (<>) {
     chomp;
-    if (length > 78) {                          # 78 Character lines
-        &style_err("line too long");
-    } elsif (/\t/) {                            # No tab characters
-        &style_err("don't use tabs");
-    } elsif (/,\S/) {                           # Space after comma
-        &style_err("put space after comma");
-    } elsif (/\/\*\S/) {                        # Space after open comment
-        &style_err("put space after open comment");
-    } elsif (/\S\*\//) {                        # Space before close comment
-        &style_err("put space before close comment");
-    } elsif (/\){/) {                           # Space between blocks
-        &style_err("put space between delimiters");
-    } elsif (/\/\//) {                          # No C++ comments
-        &style_err("don't use C++ comments");
-    } elsif (/;\S/) {                           # Space after semicolon
-        &style_err("put space after semicolon");
-                                                # Whitespace around operators
-    } elsif (/\w(\+|\-|\*|\/|<=?|>=?|==?|!=|\|\|?|\&\&?|\^)\w/) {
-        &style_err("need whitespace around operator");
-    } elsif (/\s\Z/) {                          # No trailing whitespace
-        &style_err("delete trailing whitespace");
-    }
+    &style_err("line too long") if length > 78;
+    &style_err("don't use tabs") if /\t/;
+    &style_err("put space after comma") if /,\S/;
+    &style_err("put space after open comment") if /\/\*\S/;
+    &style_err("put space before close comment") if /\S\*\//;
+    &style_err("put space between delimiters") if /\)\{/;
+    &style_err("don't use C++ comments") if /\/\//;
+    &style_err("put space after semicolon") if /;\S/;
+    &style_err("need whitespace around operator")
+            if /\w(\+|\-|\*|\/|<=?|>=?|==?|!=|\|\|?|\&\&?|\^)\w/;
+    &style_err("delete trailing whitespace") if /\s\Z/;
 } continue {
     close ARGV if eof;
 }
